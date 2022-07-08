ff8ffedb46825df37d71f1d3df0b4bf391b258e0
Koushik Sadhukhan <koushik.sadhukhan0004@gmail.com>
2022-07-08 17:51:22 +0530

    implementing bcrypt for password hashing before saving

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
   {
@@ -26,6 +27,13 @@ const userSchema = mongoose.Schema(
     timestamps: true
   }
 );
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
